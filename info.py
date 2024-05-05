b493b78a18a3ba11108237e94e5e4367d10a9fc5
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2024-05-05 09:06:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://theamanchaudhary:updatesbyaman@cluster0.qm9ulli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filesg')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'negiFiles')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
